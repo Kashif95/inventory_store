b2b0464068f3411f3023028dbaa17b97a1b828a7
Biotech822 <48480721+Biotech822@users.noreply.github.com>
2019-04-27 16:01:57 +0500

    5 screen done

--- app/src/main/java/com/meeranlabs/inventory/inventory_store/MainActivity.java
+++ app/src/main/java/com/meeranlabs/inventory/inventory_store/MainActivity.java
@@ -7,24 +7,24 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button gen, nextbtn,btnexport,btndrive,reset;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button gen, next_btn;
+        next_btn = (Button) findViewById(R.id.nxt_btn);
 
-        nextbtn = (Button) findViewById(R.id.nxt_btn);
 
-
-        nextbtn.setOnClickListener(new View.OnClickListener() {
+        next_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
 //                if(sp1.getSelectedItem().toString()!="Please Select Location") {
                 Intent rIntent = new Intent(MainActivity.this,Second.class);
-                rIntent.putExtra("key","sdfsdf");
+//                rIntent.putExtra("key","sdfsdf");
                 //  rIntent.putExtra("key", sp1.getSelectedItem().toString());
                 startActivity(rIntent);
 //                }
--- app/src/main/java/com/meeranlabs/inventory/inventory_store/Second.java
+++ app/src/main/java/com/meeranlabs/inventory/inventory_store/Second.java
@@ -1,13 +1,39 @@
 package com.meeranlabs.inventory.inventory_store;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Second extends AppCompatActivity {
-
+    Button gen, nextbtn2,btnexport,btndrive,reset;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
+        nextbtn2 = (Button) findViewById(R.id.nxt_btn2);
+
+
+        nextbtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                if(sp1.getSelectedItem().toString()!="Please Select Location") {
+                Intent rIntent = new Intent(Second.this,Third.class);
+                rIntent.putExtra("key","sdfsdf");
+                //  rIntent.putExtra("key", sp1.getSelectedItem().toString());
+                startActivity(rIntent);
+//                }
+//                else
+//                    Toast.makeText(getApplicationContext(),"Please Select Location", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+
     }
 }
