216c2fde4812f1991e71816f8e94cc0e4d03bb74
Biotech822 <48480721+Biotech822@users.noreply.github.com>
2019-04-27 14:04:28 +0500

    commit 2 screen

--- app/src/main/java/com/meeranlabs/inventory/inventory_store/MainActivity.java
+++ app/src/main/java/com/meeranlabs/inventory/inventory_store/MainActivity.java
@@ -1,13 +1,44 @@
 package com.meeranlabs.inventory.inventory_store;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button gen, nextbtn,btnexport,btndrive,reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        nextbtn = (Button) findViewById(R.id.nxt_btn);
+
+
+        nextbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//                if(sp1.getSelectedItem().toString()!="Please Select Location") {
+                Intent rIntent = new Intent(MainActivity.this,Second.class);
+                rIntent.putExtra("key","sdfsdf");
+                //  rIntent.putExtra("key", sp1.getSelectedItem().toString());
+                startActivity(rIntent);
+//                }
+//                else
+//                    Toast.makeText(getApplicationContext(),"Please Select Location", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+
     }
+
+
+
+
 }
